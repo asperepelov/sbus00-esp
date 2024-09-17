e7ee4b793dc6f308610ad9e98132bf22d5bf2343
Perepelov Alexander <asperepelov@mail.ru>
2024-09-17 20:31:08 +0300

    Вывод значений по каналам

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
-// #include <HardwareSerial.h>
 
-// HardwareSerial Serial2(2);  // Создаем объект Serial2 для UART2
 #define SBUS_RX_PIN 16 // GPIO16 (RX2)
 #define SBUS_TX_PIN 17 // GPIO17 (TX2)
 #define SBUS_BAUDRATE 100000
 #define SBUS_CONFIG SERIAL_8E2
 #define SBUS_FRAME_SIZE 25
 #define SBUS_NUM_CHANNELS 16
+
+// Массив для хранения значений каналов
+uint16_t channels[SBUS_NUM_CHANNELS];
 
 void setup() {
   pinMode(SBUS_RX_PIN, INPUT_PULLUP);
@@ -28,6 +29,38 @@ void setup() {
 // Функция для инверсии всех битов в байте
 uint8_t invertByte(uint8_t byte) {
   return ~byte;
+}
+
+// Декодирование каналов SBUS
+void decodeChannels(uint8_t* frame) {
+  channels[0]  = ((frame[1]    |frame[2]<<8)                 & 0x07FF);
+  channels[1]  = ((frame[2]>>3 |frame[3]<<5)                 & 0x07FF);
+  channels[2]  = ((frame[3]>>6 |frame[4]<<2 |frame[5]<<10)   & 0x07FF);
+  channels[3]  = ((frame[5]>>1 |frame[6]<<7)                 & 0x07FF);
+  channels[4]  = ((frame[6]>>4 |frame[7]<<4)                 & 0x07FF);
+  channels[5]  = ((frame[7]>>7 |frame[8]<<1 |frame[9]<<9)    & 0x07FF);
+  channels[6]  = ((frame[9]>>2 |frame[10]<<6)                & 0x07FF);
+  channels[7]  = ((frame[10]>>5|frame[11]<<3)                & 0x07FF);
+  channels[8]  = ((frame[12]   |frame[13]<<8)                & 0x07FF);
+  channels[9]  = ((frame[13]>>3|frame[14]<<5)                & 0x07FF);
+  channels[10] = ((frame[14]>>6|frame[15]<<2|frame[16]<<10)  & 0x07FF);
+  channels[11] = ((frame[16]>>1|frame[17]<<7)                & 0x07FF);
+  channels[12] = ((frame[17]>>4|frame[18]<<4)                & 0x07FF);
+  channels[13] = ((frame[18]>>7|frame[19]<<1|frame[20]<<9)   & 0x07FF);
+  channels[14] = ((frame[20]>>2|frame[21]<<6)                & 0x07FF);
+  channels[15] = ((frame[21]>>5|frame[22]<<3)                & 0x07FF);
+}
+
+void printChannels() {
+  for (int i = 0; i < SBUS_NUM_CHANNELS; i++) {
+    Serial.print("C");
+    Serial.print(i + 1);
+    Serial.print(":");
+    Serial.print(channels[i]);
+    Serial.print(" ");
+    // if ((i + 1) % 4 == 0) Serial.println();
+  }
+  Serial.println();
 }
 
 void loop() {
@@ -54,13 +87,20 @@ void loop() {
       // writeByte = invertByte(0x00);
       Serial2.write(&writeByte, 1);
 
-      for (int i = 0; i < SBUS_FRAME_SIZE; i++) {
-        Serial.print(i + 1);
-        Serial.print(":");
-        Serial.print(sbusFrame[i], HEX);
-        Serial.print(" ");
-      }
-      Serial.println();
+      // Декодируем каналы
+      decodeChannels(sbusFrame);
+
+      // Выводим значения каналов
+      printChannels();
+      
+      // Выводим сырые данные кадра
+      // for (int i = 0; i < SBUS_FRAME_SIZE; i++) {
+      //   Serial.print(i + 1);
+      //   Serial.print(":");
+      //   Serial.print(sbusFrame[i], HEX);
+      //   Serial.print(" ");
+      // }
+      // Serial.println();
 
       sbusIndex = 0;
       lastFrameTime = millis();
